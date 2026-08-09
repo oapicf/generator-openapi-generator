69d9a4c83d3652ee603689c60a9f016da5b1b643
Cliffano Subagio <cliffano@gmail.com>
2026-08-09 11:12:51 +1000

    Add -partials generators.

--- plopfile.js
+++ plopfile.js
@@ -28,45 +28,57 @@ export default function (plop) {
     return text.toLowerCase().replace(/ /g, '_');
   });
 
+  const prompts = [
+    {
+      type: 'input',
+      name: 'project_id',
+      message: 'Project ID'
+    },
+    {
+      type: 'input',
+      name: 'project_name',
+      message: 'Project Name'
+    },
+    {
+      type: 'input',
+      name: 'project_desc',
+      message: 'Project Description '
+    },
+    {
+      type: 'input',
+      name: 'author_name',
+      message: 'Author Name'
+    },
+    {
+      type: 'input',
+      name: 'author_email',
+      message: 'Author Email'
+    },
+    {
+      type: 'input',
+      name: 'author_url',
+      message: 'Author URL'
+    },
+    {
+      type: 'input',
+      name: 'github_id',
+      message: 'GitHub ID'
+    },
+    {
+      type: 'input',
+      name: 'github_repo',
+      message: 'GitHub Repository'
+    },
+    {
+      type: 'input',
+      name: 'github_token_prefix',
+      message: 'GitHub Actions token prefix'
+    }
+  ];
+
   plop.setGenerator('oag-file-spec', {
       description: 'OAG File Spec Plop',
-      prompts: [
-        {
-          type: 'input',
-          name: 'project_id',
-          message: 'Project ID'
-        },
-        {
-          type: 'input',
-          name: 'project_name',
-          message: 'Project Name'
-        },
-        {
-          type: 'input',
-          name: 'project_desc',
-          message: 'Project Description '
-        },
-        {
-          type: 'input',
-          name: 'author_name',
-          message: 'Author Name'
-        },
-        {
-          type: 'input',
-          name: 'author_email',
-          message: 'Author Email'
-        },
-        {
-          type: 'input',
-          name: 'author_url',
-          message: 'Author URL'
-        },
-        {
-          type: 'input',
-          name: 'github_id',
-          message: 'GitHub ID'
-        }
-      ],
+      prompts: prompts,
       actions: [
         {
           type: 'addMany',
@@ -74,6 +86,7 @@ export default function (plop) {
           templateFiles: [
             'templates/oag-file-spec/.*/*/*/*',
             'templates/oag-file-spec/.*/*/*',
+            'templates/oag-file-spec/.*/*',
             'templates/oag-file-spec/**/.*',
             'templates/oag-file-spec/**/*'
           ]
@@ -81,45 +94,24 @@ export default function (plop) {
       ]
   });
 
+  plop.setGenerator('oag-file-spec-partials', {
+    description: 'OAG File Spec partials template',
+    prompts: prompts,
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'stage/oag-file-spec-partials',
+        templateFiles: [
+          'templates/oag-file-spec-partials/*'
+        ],
+        base: 'templates/oag-file-spec-partials'
+      }
+    ]
+  });
+
   plop.setGenerator('oag-url-spec', {
     description: 'OAG URL Spec Plop',
-    prompts: [
-      {
-        type: 'input',
-        name: 'project_id',
-        message: 'Project ID'
-      },
-      {
-        type: 'input',
-        name: 'project_name',
-        message: 'Project Name'
-      },
-      {
-        type: 'input',
-        name: 'project_desc',
-        message: 'Project Description '
-      },
-      {
-        type: 'input',
-        name: 'author_name',
-        message: 'Author Name'
-      },
-      {
-        type: 'input',
-        name: 'author_email',
-        message: 'Author Email'
-      },
-      {
-        type: 'input',
-        name: 'author_url',
-        message: 'Author URL'
-      },
-      {
-        type: 'input',
-        name: 'github_id',
-        message: 'GitHub ID'
-      }
-    ],
+    prompts: prompts,
     actions: [
       {
         type: 'addMany',
@@ -127,10 +119,26 @@ export default function (plop) {
         templateFiles: [
           'templates/oag-url-spec/.*/*/*/*',
           'templates/oag-url-spec/.*/*/*',
+          'templates/oag-url-spec/.*/*',
           'templates/oag-url-spec/**/.*',
           'templates/oag-url-spec/**/*'
         ]
       }
     ]
-});
+  });
+
+  plop.setGenerator('oag-url-spec-partials', {
+    description: 'OAG URL Spec partials template',
+    prompts: prompts,
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'stage/oag-url-spec-partials',
+        templateFiles: [
+          'templates/oag-url-spec-partials/*'
+        ],
+        base: 'templates/oag-url-spec-partials'
+      }
+    ]
+  });
 };
